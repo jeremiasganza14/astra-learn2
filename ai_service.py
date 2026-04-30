362a07d7badb3ff64385b6fcf6f9d130adbdcb21
jeremiasganza14 <jeremiasganza14@gmail.com>
2026-04-30 01:32:48 -0300

    Add files via upload

--- ai_service.py
+++ ai_service.py
@@ -3,7 +3,7 @@ import json
 import fitz  # PyMuPDF
 import google.generativeai as genai
 
-API_KEY = "AIzaSyDqg20EJ2Ad2-d3vJotM6aAdn6-3Efs3pg"
+API_KEY = os.environ.get("GEMINI_API_KEY")
 if API_KEY:
     genai.configure(api_key=API_KEY)
 
